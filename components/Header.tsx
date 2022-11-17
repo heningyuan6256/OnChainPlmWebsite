5e23807429bfbd749e9ebca10c2b801ca56c6225
heningyuan <810151439@qq.com>
2022-11-17 15:55:33 +0800

    fix

--- components/Header.tsx
+++ components/Header.tsx
@@ -17,41 +17,40 @@ const Header: FC = () => {
     {
       label: "Navigation Two",
       key: "app",
-      disabled: true,
     },
     {
       label: "Navigation Three - Submenu",
       key: "SubMenu",
-      children: [
-        {
-          type: "group",
-          label: "Item 1",
-          children: [
-            {
-              label: "Option 1",
-              key: "setting:1",
-            },
-            {
-              label: "Option 2",
-              key: "setting:2",
-            },
-          ],
-        },
-        {
-          type: "group",
-          label: "Item 2",
-          children: [
-            {
-              label: "Option 3",
-              key: "setting:3",
-            },
-            {
-              label: "Option 4",
-              key: "setting:4",
-            },
-          ],
-        },
-      ],
+      // children: [
+      //   {
+      //     type: "group",
+      //     label: "Item 1",
+      //     children: [
+      //       {
+      //         label: "Option 1",
+      //         key: "setting:1",
+      //       },
+      //       {
+      //         label: "Option 2",
+      //         key: "setting:2",
+      //       },
+      //     ],
+      //   },
+      //   {
+      //     type: "group",
+      //     label: "Item 2",
+      //     children: [
+      //       {
+      //         label: "Option 3",
+      //         key: "setting:3",
+      //       },
+      //       {
+      //         label: "Option 4",
+      //         key: "setting:4",
+      //       },
+      //     ],
+      //   },
+      // ],
     },
     {
       label: (
