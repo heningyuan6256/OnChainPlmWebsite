19316a603cf7cb9981c4dc2f1117e7f09d098d17
heningyuan <810151439@qq.com>
2022-11-17 15:35:21 +0800

    fix bug

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,79 @@
-import { FC } from "react";
+import { FC, useState } from "react";
+import styles from "../styles/Header.module.less";
+import { MenuProps, Menu } from "antd";
 
 const Header: FC = () => {
-  return <div></div>;
+  const [current, setCurrent] = useState("mail");
+  const onClick: MenuProps["onClick"] = (e) => {
+    console.log("click ", e);
+    setCurrent(e.key);
+  };
+
+  const items: MenuProps["items"] = [
+    {
+      label: "Navigation One",
+      key: "mail",
+    },
+    {
+      label: "Navigation Two",
+      key: "app",
+      disabled: true,
+    },
+    {
+      label: "Navigation Three - Submenu",
+      key: "SubMenu",
+      children: [
+        {
+          type: "group",
+          label: "Item 1",
+          children: [
+            {
+              label: "Option 1",
+              key: "setting:1",
+            },
+            {
+              label: "Option 2",
+              key: "setting:2",
+            },
+          ],
+        },
+        {
+          type: "group",
+          label: "Item 2",
+          children: [
+            {
+              label: "Option 3",
+              key: "setting:3",
+            },
+            {
+              label: "Option 4",
+              key: "setting:4",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      label: (
+        <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
+          Navigation Four - Link
+        </a>
+      ),
+      key: "alipay",
+    },
+  ];
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <Menu
+          onClick={onClick}
+          selectedKeys={[current]}
+          mode="horizontal"
+          items={items}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
