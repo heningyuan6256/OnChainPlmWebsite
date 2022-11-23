545c3c58d5339da5de5b92b2813d62efc8cbcb4b
heningyuan <810151439@qq.com>
2022-11-23 17:05:11 +0800

    fix bug

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,6 +21,16 @@ const AdvertiseBox: FC = (props) => {
             产品全生命周期赋能平台
           </span>
         </div>
+        <div className={styles.advertise_box_sub_title}>
+          简化产品开发并在云中快速交付创新产品
+        </div>
+        <div className={styles.advertise_box_sub_title}>
+          探索OnChain云原生产品生命周期管理(PLM)解决方案的优势
+        </div>
+        <div className={styles.advertise_box_sub_button_group}>
+          <Button>申请试用</Button>
+          <Button>视频介绍</Button>
+        </div>
       </div>
     </div>
   );
@@ -39,46 +49,6 @@ export default function Home({ posts }: any) {
 
       <main className={styles.main}>
         <AdvertiseBox></AdvertiseBox>
-        {/* <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div> */}
       </main>
       <Footer></Footer>
     </div>
