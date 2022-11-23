cb73987242ed119f8da627bdb4138417a19e1b96
heningyuan <810151439@qq.com>
2022-11-23 15:15:38 +0800

    fix bug

--- next.config.js
+++ next.config.js
@@ -1,16 +1,3 @@
-// /** @type {import('next').NextConfig} */
-// const withLess = require("next-with-less");
-// const nextConfig = withLess({
-//   reactStrictMode: true,
-//   swcMinify: true,
-//   images: {
-//     loader: "akamai",
-//     path: "",
-//   },
-// });
-
-// module.exports = nextConfig;
-
 const withAntdLess = require("next-plugin-antd-less");
 
 module.exports = withAntdLess({
