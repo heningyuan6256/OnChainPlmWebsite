910816a8c5285959a193a63f3b113d1719849645
heningyuan <810151439@qq.com>
2022-11-23 15:25:25 +0800

    fix bug

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const withAntdLess = require("next-plugin-antd-less");
 
 module.exports = withAntdLess({
   // 修改antd的主题变量
-  modifyVars: { "@primary-color": "#333", "@ant-prefix": "plm" }, // 修改antd的primary颜色为黑色
+  modifyVars: { "@primary-color": "#15171A", "@ant-prefix": "plm" }, // 修改antd的primary颜色为黑色
   // optional https://github.com/webpack-contrib/css-loader#object
   // cssLoaderOptions: {},
   reactStrictMode: true,
