f6c7bd5b6ed5eb71f1e9153d151f112f82c6aca8
heningyuan <810151439@qq.com>
2022-12-10 10:34:33 +0800

    fix bug

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -34,50 +34,48 @@ const Footer: FC = () => {
           <div className={styles.button}>
             <Button type="primary">订阅</Button>
           </div>
-          {/* <div className={styles.reference}>
-            <span>
-              Copyright &#169 2012-2022-dahai information system technology
-              cd.ltd
-            </span>
-          </div> */}
           <div className={styles.reference}>
             <span>鄂ICP备15020218号-1</span>
           </div>
         </div>
-        <div className={styles.box2}>
-          <div className={styles.title}>产品</div>
-          <div className={styles.row}>事件</div>
-          <div className={styles.row}>更新日志</div>
-          <div className={styles.row}>网站地图</div>
-          <div className={styles.row}>隐私政策</div>
-          <div className={styles.row}>与华为云的合作</div>
-        </div>
-        <div className={styles.box3}>
-          <div className={styles.title}>服务</div>
-          <div className={styles.row}>解决方案</div>
-          <div className={styles.row}>技术博客</div>
-          <div className={styles.row}>帮助中心</div>
-        </div>
-        <div className={styles.box4}>
-          <div className={styles.title}>业务支持</div>
-          <div className={styles.row}>027-88319536</div>
-          <div className={styles.row}>postmaster@dahaikj.com</div>
-          <div className={styles.title} style={{ margin: "32px 0px 30px 0px" }}>
-            联系我们
+        <div className={styles.box_container}>
+          <div style={{ display: "flex" }}>
+            <div className={styles.box2}>
+              <div className={styles.title}>产品</div>
+              <div className={styles.row}>事件</div>
+              <div className={styles.row}>更新日志</div>
+              <div className={styles.row}>网站地图</div>
+              <div className={styles.row}>隐私政策</div>
+              <div className={styles.row}>与华为云的合作</div>
+            </div>
+            <div className={styles.box3}>
+              <div className={styles.title}>服务</div>
+              <div className={styles.row}>解决方案</div>
+              <div className={styles.row}>技术博客</div>
+              <div className={styles.row}>帮助中心</div>
+            </div>
+            <div className={styles.box4}>
+              <div className={styles.title}>业务支持</div>
+              <div className={styles.row}>027-88319536</div>
+              <div className={styles.row}>postmaster@dahaikj.com</div>
+              <div style={{ display: "flex", gap: "20px" }}>
+                <AliIconFont
+                  type="icon-front-weixin"
+                  style={{ color: "#528ff4", fontSize: "24px" }}
+                ></AliIconFont>
+                <AliIconFont
+                  type="icon-front-qq"
+                  style={{ fontSize: "24px", color: "#A7B3C5" }}
+                ></AliIconFont>
+                <AliIconFont
+                  type="icon-front-github"
+                  style={{ fontSize: "24px", color: "#A7B3C5" }}
+                ></AliIconFont>
+              </div>
+            </div>
           </div>
-          <div style={{ display: "flex", gap: "20px" }}>
-            <AliIconFont
-              type="icon-front-weixin"
-              style={{ color: "#528ff4", fontSize: "24px" }}
-            ></AliIconFont>
-            <AliIconFont
-              type="icon-front-qq"
-              style={{ fontSize: "24px" }}
-            ></AliIconFont>
-            <AliIconFont
-              type="icon-front-github"
-              style={{ fontSize: "24px" }}
-            ></AliIconFont>
+          <div className={styles.box_bottom}>
+            Copyright &#169 2012-2022-dahai information system technology cd.ltd
           </div>
         </div>
       </div>
