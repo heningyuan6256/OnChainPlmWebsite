c557f5e19458275403c691f9c39aef7372f4c716
heningyuan <810151439@qq.com>
2022-12-15 15:38:16 +0800

    fix bug

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,38 +10,38 @@ export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
   const [loading, setLoading] = useState(false);
   useEffect(() => {
-    router.events.on("routeChangeStart", () => {
-      setLoading(true);
-    });
-    router.events.on("routeChangeComplete", () => {
-      setLoading(false);
-    });
-    router.events.on("routeChangeError", () => {
-      setLoading(false);
-    });
+    // router.events.on("routeChangeStart", () => {
+    //   setLoading(true);
+    // });
+    // router.events.on("routeChangeComplete", () => {
+    //   setLoading(false);
+    // });
+    // router.events.on("routeChangeError", () => {
+    //   setLoading(false);
+    // });
   }, []);
-  if (loading) {
-    return (
-      <div
-        className={`plm_loading ${ScrollAnimateClass} animate__fadeOut animate__fadeIn`}
-      >
-        <div className={`jump`}>
-          <Image
-            src={"/front-plm loaidng logo.svg"}
-            alt=""
-            width={64}
-            height={64}
-          ></Image>
-          <Image
-            src={"/front-onchainnew2.svg"}
-            alt=""
-            width={160}
-            height={33}
-          ></Image>
-        </div>
-      </div>
-    );
-  }
+  // if (loading) {
+  //   return (
+  //     <div
+  //       className={`plm_loading ${ScrollAnimateClass} animate__fadeOut animate__fadeIn`}
+  //     >
+  //       <div className={`jump`}>
+  //         <Image
+  //           src={"/front-plm loaidng logo.svg"}
+  //           alt=""
+  //           width={64}
+  //           height={64}
+  //         ></Image>
+  //         <Image
+  //           src={"/front-onchainnew2.svg"}
+  //           alt=""
+  //           width={160}
+  //           height={33}
+  //         ></Image>
+  //       </div>
+  //     </div>
+  //   );
+  // }
   return (
     <ConfigProvider prefixCls="plm">
       <Component {...pageProps} />
